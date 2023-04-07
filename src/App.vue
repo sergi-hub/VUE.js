473e78328f726baa4c64f53815aadd887ca6ba89
sergi <sergisignes2002@gmail.com>
2023-04-07 16:35:17 +0200

    prueba1

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/favoritos">Favoritos</RouterLink>
       </nav>
     </div>
+    <p>p</p>
   </header>
 
   <RouterView />
