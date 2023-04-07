b256fd078c2479442a6110fd97ee888498dc8559
sergi <sergisignes2002@gmail.com>
2023-04-07 18:00:53 +0200

    titulos

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Casa</RouterLink>
+        <RouterLink to="/about">Sobre nosotros</RouterLink>
         <RouterLink to="/pokedex">Pokedex</RouterLink>
         <RouterLink to="/favoritos">Favoritos</RouterLink>
       </nav>
